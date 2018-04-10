358d543d390bc8d4182bd016c86863408b1f6de7
flourishcodes <kunle@adeniran.com>
2018-04-10 13:14:17 -0700

    rewrote the entire code to corrent indentation

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range (0, 10):
-  for k in range (0, 10):
-    if i < k:
-      if i < 8:
-        print("{}{}, ".format(i,k), end="")
-      else:
-          print("{}{}".format(i,k))
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i < j:
+            if i < 8:
+                print("{}{}, ".format(i, j), end="")
+            else:
+                    print("{}{}".format(i, j))
