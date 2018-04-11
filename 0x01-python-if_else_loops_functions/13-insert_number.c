9ee05b5111680f41ae2d29bb48f1ef2dff5e2ef4
flourishcodes <kunle@adeniran.com>
2018-04-10 22:28:47 -0700

    test check

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -18,11 +18,23 @@ listint_t *insert_node(listint_t **head, int number)
 	node->next = NULL;
 
 	temp = *head;
-	while (number >= temp->next->n)
+	while (temp->next != NULL)
 	{
+		if (number < temp->n)
+		{
+			node->next = temp;
+			*head = node;
+			return (node);
+		}
+		if (number <=temp->next->n)
+		{
+			node->next = temp->next;
+			temp->next = node;
+			return (node);
+		}
 		temp = temp->next;
 	}
-	node->next = temp->next;
+	node->next = NULL;
 	temp->next = node;
 	return (node);
 }
