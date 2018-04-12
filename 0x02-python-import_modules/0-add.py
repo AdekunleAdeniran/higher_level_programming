60ed1de92335c18e64a53a99e8341e4bee861d1a
flourishcodes <kunle@adeniran.com>
2018-04-12 13:02:40 -0700

    edited add script with if statement to check file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-from add_0 import add
-a = 1
-b = 2
-c = a + b
-print("{} + {} = {}".format(a, b, c))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    c = a + b
+    print("{} + {} = {}".format(a, b, c))
