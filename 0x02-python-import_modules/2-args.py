1ee56e7312547ee6f1ca911bbdaae4532c2a5b91
flourishcodes <kunle@adeniran.com>
2018-04-12 17:12:42 -0700

    argument function

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print("{} arguments.".format(argc - 1))
     else:
         if argc == 2:
-            print("{} argument:".format(argc -1))
+            print("{} argument:".format(argc - 1))
         else:
             print("{} arguments:".format(argc - 1))
     for n in range(1, argc):
