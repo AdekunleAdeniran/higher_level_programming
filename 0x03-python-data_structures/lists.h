c80c537dd761676cd546e2ac862d3f83cd5a17bc
flourishcodes <kunle@adeniran.com>
2018-04-18 01:24:04 -0700

    add reverse list function

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -20,5 +20,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+listint_t *reverse_listint(listint_t **head);
 
 #endif /* LISTS_H */
