cb1bd857a36def20a94c2714d797e8a535d2c583
flourishcodes <kunle@adeniran.com>
2018-04-17 21:43:46 -0700

    Python function to check if number is divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for n in my_list:
-        if n % 2 == 0:
-            return True
-        return False
+    return(x % 2 == 0 for x in my_list)
