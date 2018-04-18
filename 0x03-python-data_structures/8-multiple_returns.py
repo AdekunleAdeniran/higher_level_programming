e28ab01f1f8b7cc4bdd7b662b846c0a87562bf56
flourishcodes <kunle@adeniran.com>
2018-04-17 21:21:52 -0700

    Return length of sentence and first letter in sentence

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         first_letter = None
     else:
-        first_letter  = sentence[0]
+        first_letter = sentence[0]
     return len(sentence), first_letter
