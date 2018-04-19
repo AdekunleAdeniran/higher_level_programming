92c8136a8fc49b5418cba8a062e4691518ca7fc9
flourishcodes <kunle@adeniran.com>
2018-04-19 15:59:22 -0700

    Python function to search and replace a value in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,8 @@
 def search_replace(my_list, search, replace):
     new = []
     for n in my_list:
-       if n == search:
-           new.append(replace)
-       else:
-           new.append(n)
+        if n == search:
+            new.append(replace)
+        else:
+            new.append(n)
     return(new)
