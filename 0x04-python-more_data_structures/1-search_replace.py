890a6539edf4c041c8d3115944034cd41e49108f
flourishcodes <kunle@adeniran.com>
2018-04-19 15:57:29 -0700

    Python function to search and replace a value in a list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new = []
+    for n in my_list:
+       if n == search:
+           new.append(replace)
+       else:
+           new.append(n)
+    return(new)
