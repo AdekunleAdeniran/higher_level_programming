143fedba824eb3973910dd69d62fccbf7a314c55
flourishcodes <kunle@adeniran.com>
2018-04-19 15:56:34 -0700

    Python function to square the elements of a matrix and return original Matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix:
+        new = []
+        for rows in matrix:
+            new.append([n ** 2 for n in rows])
+        return new
