6e9665d5ead3ea92277c996371761786b41f6789
flourishcodes <kunle@adeniran.com>
2018-04-20 09:16:08 -0700

    Python Funtion to convert Roman Numbers to Integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,13 @@
 def roman_to_int(roman_string):
     dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
-    items = 0
-    if type(roman_string) is not str or len(roman_string) is 0:
+    item = 0
+    roman = roman_string
+    if type(roman) is not str or len(roman) is 0:
         return 0
-    for items in range(items, len(roman_string)):
-        if items < len(roman_string) -1 and dic[roman_string[items]] < dic[roman_string[items + 1]]:
-            result -= dic[roman_string[items]]
+    for item in range(item, len(roman)):
+        if item < len(roman) - 1 and dic[roman[item]] < dic[roman[item + 1]]:
+            result -= dic[roman[item]]
         else:
-            result += dic[roman_string[items]]
+            result += dic[roman[item]]
     return result
