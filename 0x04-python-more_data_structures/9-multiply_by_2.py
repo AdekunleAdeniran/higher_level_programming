4649e06852618a333628811f163b159883798b25
flourishcodes <kunle@adeniran.com>
2018-04-19 20:45:43 -0700

    Python function to multiply the elements of a dictionary by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for new in a_dictionary:
-        a_dictionary[new] *= 2
-    return a_dictionary
+    new = a_dictionary.copy()
+    for ne in new:
+        new[ne] *= 2
+    return new
