e4202eecaf3c32932ae3fd002af105b225ff1eff
flourishcodes <kunle@adeniran.com>
2018-04-30 20:57:05 -0700

    Python function that divids elements in two lists and returns new list, checking errors

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,18 +1,19 @@
-#!/usr/bin/python3
+#!/bin/usr/python3
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
+    new = []
+    result = 0
     for items in range(list_length):
         try:
-            new_list.append(my_l_1[items] / my_l_2[items])
+            result = my_list_1[items] / my_list_2[items]
         except TypeError:
             print("wrong type")
-            new_list.append(0)
+            result = 0
         except ZeroDivisionError:
             print("division by 0")
-            new_list.append(0)
+            result = 0
         except IndexError:
             print("out of range")
-            new_list.append(0)
+            result = 0
         finally:
-            pass
-        return new_list
+            new.append(result)
+    return new
