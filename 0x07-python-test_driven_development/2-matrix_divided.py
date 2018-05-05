a8c72f4c02a2c906bc9d89d87312b8aa495bf4ae
flourishcodes <kunle@adeniran.com>
2018-05-04 20:47:49 -0700

    Python function to divide the items of a matrix with a variable

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+
+'''
+    2-matrix_divided.py
+
+    Contains function that divides all elements of a matrix
+'''
+
+
+def matrix_divided(matrix, div):
+    '''
+    Python Function that divide a matrix by variable div
+    '''
+
+    listError = 'matrix must be a matrix (list of lists) of integers/floats'
+    # Check if matrix is a list
+    if type(matrix) is not list:
+        raise TypeError(listError)
+
+    # Check if rows in matrix is list
+    for rows in matrix:
+        if type(rows) is not list:
+            raise TypeError(listError)
+
+    # Check if length of rows are the same
+    if len(matrix[0]) != len(matrix[1]):
+        raise TypeError('Each row of the matrix must have the same size')
+
+    # Check to see if div is zero
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+
+    # Check if items in matrix are floats or ints
+    for rows in matrix:
+        for items in rows:
+            if not isinstance (items, (int, float)):
+                raise TypeError(listError)
+
+    # Python Function to divide elements of a matrix
+    new = []
+    for rows in matrix:
+        if not isinstance(div, (int, float)):
+            raise TypeError('div must be a number')
+        else:
+            new.append([round((items / div), 2) for items in rows])
+    return new
