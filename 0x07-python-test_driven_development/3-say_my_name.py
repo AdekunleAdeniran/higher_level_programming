bd9dbfc3055d522a812e95f152e561cccf42d0f7
flourishcodes <kunle@adeniran.com>
2018-05-05 12:15:14 -0700

    Python function that prints name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+'''
+   3-say_my_name
+   Contains a function that prints names
+'''
+
+
+def say_my_name(first_name, last_name=""):
+    '''
+    Python function that takes two strings and prints them
+    '''
+errmsg = 'first_name must be a string'
+errmsg1 = 'last_name must be a string'
+name = 'My name is '
+def say_my_name(first_name, last_name=""):
+  if not isinstance(first_name, str):
+    raise TypeError(errmsg)
+  if not isinstance(last_name, str):
+    raise TypeError(errmsg1)
+  else:
+    print('My name is {} {}'.format(first_name, last_name))
