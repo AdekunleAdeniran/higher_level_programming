505001275e210dc4a1154850e9063fe69c632191
flourishcodes <kunle@adeniran.com>
2018-05-05 22:26:22 -0700

    Unittest file to test max int

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Module to find the max integer
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    Test the max_integer function
+    """
+
+    def test_max_integer(self):
+        '''
+        Test if integer is max integer
+        '''
+        result = max_integer([1, 4, 3, 2])
+        self.assertEqual(result, 4)
+        result = max_integer([12, 9, 7, 2])
+        self.assertEqual(result, 12)
+
+    def test_isint(self):
+        '''
+        Test to check variable against integer
+        '''
+        var = 1
+        self.assertTrue(max_integer([var, 2]) == 2)
+        x = 5
+        self.assertTrue(max_integer([1, x]) == x)
+
+    def test_same_entry(self):
+        '''
+        Test to check max int if same
+        '''
+        a = 50
+        self.assertEqual(max_integer([a, a, a, a]), a)
+
+    def test_float_integer(self):
+        '''
+        Test to see if float is max integer
+        '''
+        self.assertEqual(max_integer([4.0, 3, 2]), 4.0)
+
+    def test_one_entry(self):
+        '''
+        Test to check if only one entry
+        '''
+        self.assertTrue(max_integer([10]))
+
+    def test_negative_integer(self):
+        '''
+        Test only negative integers
+        '''
+        self.assertEqual(max_integer([-1, -2, -5]), -1)
+
+    if __name__ == '__main__':
+        unittest.main()
