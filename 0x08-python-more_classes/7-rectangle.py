49d35c104e01c15cfaa2edb1c8dbfaf0cd121c5a
flourishcodes <kunle@adeniran.com>
2018-05-07 21:04:57 -0700

    Change print representation to different variables

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+'''7-rectangle: Python Funtion that defines Rectangle type'''
+
+
+class Rectangle:
+    '''Defines the Rectangle type'''
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return""
+        else:
+            for h in range(self.__height):
+                for w in range(self.height - 1):
+                    print(str(self.print_symbol) * self.__width)
+                return str(self.print_symbol) * self.width
+
+    def __repr__(self):
+        return("Rectangle({}, {})".format(self.width, self.height))
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            perimeter = 0
+        else:
+            return (2 * (self.height + self.width))
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle…")
