e4151e325079a4fdec5ae89fc7e87ad4423bb9a6
flourishcodes <kunle@adeniran.com>
2018-05-15 17:12:28 -0700

    change type check for function

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,7 +16,7 @@ class BaseGeometry:
         '''
         check value input is correct type
         '''
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
