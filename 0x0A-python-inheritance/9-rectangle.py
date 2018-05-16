38730e84d6d7898b903103d84d14494354de67de
flourishcodes <kunle@adeniran.com>
2018-05-16 09:38:04 -0700

    fixed pep

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -3,6 +3,7 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     '''class Rectangle inherits from BaseGeometry'''
 
@@ -10,13 +11,17 @@ class Rectangle(BaseGeometry):
         '''private instantiation of attributes
            validate type with integer_validator
         '''
-        super().integer_validator('width', width)
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
         self.__width = width
-        super().integer_validator('height', height)
         self.__height = height
 
     def __str__(self):
         '''informal representation of a Rectangle'''
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def __repr__(self):
+        '''formal representation of a Rectangle'''
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
     def area(self):
