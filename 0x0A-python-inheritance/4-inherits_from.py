450269bf4d10db20272af399e29ed50c9183dd4c
flourishcodes <kunle@adeniran.com>
2018-05-16 01:01:16 -0700

    corrected comparison of type

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     '''
     Returns True or False
     '''
-    return isinstance(obj, a_class) and not isinstance(obj, a_class)
+    return isinstance(obj, a_class) and a_class is not type(obj)
