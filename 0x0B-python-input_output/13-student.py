2fbf679353230757d6700c661c1dbdf5cdbbcd10
flourishcodes <kunle@adeniran.com>
2018-05-16 21:51:07 -0700

    Python Functions for input output json

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Python function to create class Student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        my_dict = {}
+        for items in attrs:
+            if hasattr(self, items):
+                my_dict[items] = getattr(self, items)
+        return my_dict
+
+    def reload_from_json(self, json):
+        for items in json:
+            self.__dict__[items] = json[items]
