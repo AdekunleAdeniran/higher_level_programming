7d93933e4c1e2e4d67aed8a8806301e2868331b6
flourishcodes <kunle@adeniran.com>
2018-05-22 22:47:41 -0700

    update Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,17 +21,17 @@ class Base:
     def to_json_string(list_dictionaries):
         '''Method that returns the JSON string representation of function'''
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
         '''Method that writes JSON string representation to file'''
+        new = []
+        for i in list_objs:
+            new.append(cls.to_dictionary(i))
         with open("{}.json".format(cls.__name__), 'w') as f:
-            new = []
-            for i in list_objs:
-                new.append(i.to_dictionary())
             f.write(cls.to_json_string(new))
 
     @staticmethod
@@ -45,9 +45,12 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         '''class method that prints instances'''
-        temp = cls(1, 1)
-        temp.update(**dictionary)
-        return temp
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
 
     @classmethod
     def load_from_file(cls):
