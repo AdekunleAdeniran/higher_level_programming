a27caa5a2ab12f3165e656fba9a084d14d1cc0e5
flourishcodes <kunle@adeniran.com>
2018-06-25 21:04:36 -0700

    MySql script to alter value of item

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- MySql script to alter score value of item in table
-UPDATE second_table SET score=10 where second_table.name = "Bob";
+UPDATE second_table SET score=10 WHERE second_table.name = "Bob";
