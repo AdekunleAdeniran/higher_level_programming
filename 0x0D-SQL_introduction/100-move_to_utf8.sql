1c0a05c1c14af28cbfefab4f1d85ba399dcd088f
flourishcodes <kunle@adeniran.com>
2018-06-25 22:14:54 -0700

    convert database to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,17 @@
+-- MySql to convert database to UTF8
+ALTER DATABASE
+      hbtn_0c_0
+      CHARACTER SET = utf8mb4
+      COLLATE = utf8mb4_unicode_ci;
+
+ALTER TABLE
+      first_table
+      CONVERT TO CHARACTER SET utf8mb4
+      COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE
+      first_table
+      CHANGE name name
+      VARCHAR(256)
+      CHARACTER SET utf8mb4
+      COLLATE utf8mb4_unicode_ci;
