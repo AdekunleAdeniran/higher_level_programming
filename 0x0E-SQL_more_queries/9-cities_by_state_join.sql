deabdd4d05e0d38919571333c1f8725f661ef79c
flourishcodes <kunle@adeniran.com>
2018-06-26 19:42:47 -0700

    select information from two files

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- MySQL script that list all cities
 SELECT cities.id, cities.name, states.name
-FROM states, cities WHERE cities.id = states.id
+FROM states, cities WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
