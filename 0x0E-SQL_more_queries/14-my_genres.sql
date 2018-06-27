ec2f4a9dfeda8805e7185699cf86fde15eba549b
flourishcodes <kunle@adeniran.com>
2018-06-26 22:12:47 -0700

    Search for specific in table

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- MySQL to select Genres
+SELECT name
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name ASC;
