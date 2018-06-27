a5e034b5cb931969951c595b040ab33acf6d162b
flourishcodes <kunle@adeniran.com>
2018-06-26 21:19:47 -0700

    MySql script to sort with NULL

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- MySQL to sort table by NULL
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+RIGHT JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id is NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
