1cc183e8e7ac0f1797cf8c577d26a924b6147f87
flourishcodes <kunle@adeniran.com>
2018-07-20 16:15:58 -0700

    Class

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Python Script to create a class State
+"""
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Creates the class State"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('state.id'))
