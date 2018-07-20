81b145df5982d4bb85c53b9a6ea09a9784e06819
flourishcodes <kunle@adeniran.com>
2018-07-20 16:44:21 -0700

    Create city class

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     item = session.query(City, State).filter(City.state_id == State.id)\
                                      .order_by(City.id).all()
     for city, state in item:
-        print("{}: ({}) ({})".format(state.name, city.id, city.name))
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
