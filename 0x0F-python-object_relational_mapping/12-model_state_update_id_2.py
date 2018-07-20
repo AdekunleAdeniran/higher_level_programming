1a24728e6f0e806ab6c4d7dad036537453fd67da
flourishcodes <kunle@adeniran.com>
2018-07-20 13:04:05 -0700

    Scrit to update list with SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sqlalchemy
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    item = session.query(State).filter_by(id=2).first()
+    item.name = 'New Mexico'
+    session.commit()
+    session.close()
