a850565f533486090f31cb29c81e2f232ef3a1d3
flourishcodes <kunle@adeniran.com>
2018-07-31 12:41:16 -0700

    Bash script to display content length of URL

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script to display the size of the body of URL response
-curl -sI $1 | grep -i Content-Length | cut -d ' ' -f2
+curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f2
