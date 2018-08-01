8dfd727d7f81e384174c87e994d98daf3435d5a2
flourishcodes <kunle@adeniran.com>
2018-07-31 23:19:07 -0700

    Curl json

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script to display the size of the body of URL response
-curl -sX POST "$1" -d @"$2" "Content-Type: application/json"
+curl -sX POST "$1" -d @"$2" -H "Content-Type: application/json"
