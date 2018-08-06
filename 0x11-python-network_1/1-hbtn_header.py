7b811b7aef6940d8c52fab2ea994bbd0c7f099d9
flourishcodes <kunle@adeniran.com>
2018-08-06 11:20:09 -0700

    Python script to get header values

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # Python script that displays getheader value
 
-import urllib.request as ur
-from sys import argv
+if __name__ == '__main__':
+    import urllib.request as ur
+    from sys import argv
 
-with ur.urlopen(argv[1]) as header:
-    print(header.getheader('X-Request-Id'))
+    with ur.urlopen(argv[1]) as header:
+        print(header.getheader('X-Request-Id'))
