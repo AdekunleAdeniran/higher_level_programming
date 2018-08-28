8ec6ff30769e5f82c09fed06baa18dc0aa900382
flourishcodes <kunle@adeniran.com>
2018-08-28 13:13:07 -0700

    JS to contact arguments

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-// JS to check arguments passed into script and prints first argument
+// JS to check arguments passed into script and concatnates arguments
 'use strict';
 console.log(process.argv[2] + ' is ' + process.argv[3]);
