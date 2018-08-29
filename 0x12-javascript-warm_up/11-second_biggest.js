981d18dfeca2c653632ef73d88c86d28e39c455c
flourishcodes <kunle@adeniran.com>
2018-08-28 19:49:07 -0700

    JS to find the second largest number in array

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// JS to find the second largest number
+'use strict';
+(function second_biggest(arg) {
+  let list = [];
+  if (process.argv.length < 4) {
+    console.log(0);
+  } else {
+    for (let count = 2; count < process.argv.length; count++) {
+      list.push(Number(process.argv[count]));
+    }
+  }
+  console.log(list.sort()[list.length - 2]);
+})()
