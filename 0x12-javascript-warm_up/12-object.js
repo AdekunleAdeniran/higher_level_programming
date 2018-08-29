d1a91a1992a92e943935b38c347798905b49bc9c
flourishcodes <kunle@adeniran.com>
2018-08-28 18:33:19 -0700

    Update object value in JS

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// JS to find the second largest number
+'use strict';
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
