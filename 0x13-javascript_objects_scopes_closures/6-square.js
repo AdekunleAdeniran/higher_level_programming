c1f0ddf50a43236ead7e3f6514d4c07bd9edb99e
flourishcodes <kunle@adeniran.com>
2018-09-04 16:52:12 -0700

    JS script to charPrint input or default

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// JS script to define class that inherits from another class
+const newSquare = require('./5-square');
+
+class Square extends newSquare {
+  charPrint(c) {
+    for (let count = 0; count < this.height; count++) {
+      if (c === undefined) {
+	console.log('X'.repeat(this.width));
+      }
+      else {
+	console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
