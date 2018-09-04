84c7bad0c2d0816ff2d3ec671c98dc5b65807bf8
flourishcodes <kunle@adeniran.com>
2018-09-04 13:52:24 -0700

    JS to check if w/h are positive integers

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// JS script to define an empty class
+// JS script to define a class, initialize values if positive integer
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
