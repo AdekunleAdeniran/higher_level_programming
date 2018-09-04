6ac115453f9fbfef48fc447f3f72e319a5749109
flourishcodes <kunle@adeniran.com>
2018-09-04 15:57:27 -0700

    JS to extend class REctangle to Square

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+// JS script to define a class, creates an instance method to print rectangle
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let count = 0; count < this.height; count++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+  rotate () {
+    let holder = this.width;
+    this.width = this.height;
+    this.height = holder;
+  }
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
