9cbca28e490e6d36e7527ab403a1bfca99f42d6f
flourishcodes <kunle@adeniran.com>
2018-09-04 14:04:01 -0700

    JS to print size of rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// JS script to define a class, creates an instance method to print rectangle
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let count = 0; count < this.height; count++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
