fc896993d0baf430f8cadc30618cc4605a96c6e9
flourishcodes <kunle@adeniran.com>
2018-09-04 18:10:23 -0700

    JS to reverse a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//JS FUNCTION TO REVERSE A LIST
+exports.esrever = function (list) {
+  let newList = [];
+  for (let index = list.length - 1; index >= 0; index--) {
+    newList.push(list[index]);
+  }
+  return newList;
+};
