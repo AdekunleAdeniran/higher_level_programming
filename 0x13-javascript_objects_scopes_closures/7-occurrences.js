704e6f939fb56b71dfb9760ff1807015b40b9848
flourishcodes <kunle@adeniran.com>
2018-09-04 17:07:17 -0700

    JS to print number of occurences in a list

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let index = 0; index < list.length; index++){
+    if (list[index] === searchElement){
+      count++;
+    }
+  }
+  return count;
+}
